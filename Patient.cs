d6075f3fe868db6f1adfe7a13e5f5b683c5a8403
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-10 18:15:24 +0600

    Create Patient.cs

---
+++ Patient.cs
@@ -0,0 +1,11 @@
+public class Patient
+{
+    public int id;
+    public string username;
+    public int age;
+    public string gender;
+    public string address;
+    public long contact;
+    public string history;
+    public List<Patient> patient=new List<Patient>();
+}
