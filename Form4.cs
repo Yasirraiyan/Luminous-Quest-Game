bd2ddc5fedd1ae610dc5289386583bcf48c6a4a9
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-10 18:09:52 +0600

    Create Form4.cs

---
+++ Form4.cs
@@ -0,0 +1,13 @@
+public partial class Form4 : Form
+{
+    public Form4()
+    {
+        InitializeComponent();
+    }
+
+    // Method to add item to listBox1
+    public void AddItemToListBox(string item)
+    {
+        listBox1.Items.Add(item);
+    }
+}
