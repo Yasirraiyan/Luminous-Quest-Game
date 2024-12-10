a953fdb66d7974a99df33f42cf05afe722aaa71e
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2024-12-10 18:13:46 +0600

    Create Program2.cs

---
+++ Program2.cs
@@ -0,0 +1,112 @@
+namespace Hospital_Management_System
+{
+    public class Program2cs
+    {
+        static void Main()
+        {
+
+            Character character1 = new Character("Warrior", 90, 80, 10, 1, "Armor", "Sword", 0, 0, 0);
+            Character character2 = new Character("Mage", 70, 90, 8, 2, "Robe", "Staff", 1, 1, 1);
+            Character character3 = new Character("Ranger", 80, 85, 9, 3, "Leather", "Bow", 2, 2, 2);
+            Character character4 = new Character("Assassin", 85, 70, 7, 4, "Cloak", "Dagger", 3, 3, 3);
+            Character character5 = new Character("Paladin", 95, 75, 10, 5, "Plate", "Hammer", 4, 4, 4);
+            Character character6 = new Character("Healer", 60, 95, 8, 6, "Gown", "Relic", 5, 5, 5);
+            Character character7 = new Character("Necromancer", 65, 92, 6, 7, "Shroud", "Orb", 6, 6, 6);
+            Character character8 = new Character("Knight", 88, 78, 9, 8, "Chainmail", "Lance", 7, 7, 7);
+            Character character9 = new Character("Bard", 75, 88, 8, 9, "Tunic", "Lute", 8, 8, 8);
+            Character character10 = new Character("Monk", 77, 87, 7, 10, "Robe", "Scroll", 9, 9, 9);
+            Character character11 = new Character("Druid", 66, 89, 7, 11, "Robes", "Totem", 10, 10, 10);
+            Character character12 = new Character("Berserker", 93, 68, 6, 12, "Fur", "Axe", 11, 11, 11);
+            Character character13 = new Character("Sorcerer", 74, 92, 5, 13, "Cloak", "Wand", 12, 12, 12);
+            Character character14 = new Character("Thief", 72, 76, 8, 14, "Leather", "Knife", 13, 13, 13);
+            Character character15 = new Character("Guardian", 89, 81, 10, 15, "Plate", "Shield", 14, 14, 14);
+            Character character16 = new Character("Shaman", 67, 90, 7, 16, "Hide", "Totem", 15, 15, 15);
+            Character character17 = new Character("Hunter", 79, 82, 9, 17, "Leather", "Bow", 16, 16, 16);
+            Character character18 = new Character("Priest", 62, 94, 6, 18, "Vestment", "Mace", 17, 17, 17);
+            Character character19 = new Character("Alchemist", 68, 91, 7, 19, "Robes", "Potion", 18, 18, 18);
+            Character character20 = new Character("Warlock", 80, 83, 5, 20, "Robes", "Grimoire", 19, 19, 19);
+            // ২০টি Weather অবজেক্ট তৈরি করা এবং প্রতিটি মেথড প্রয়োগ করা
+            Weather weather1 = new Weather();
+            weather1.UpdateWeather();
+            weather1.DisplayWeather();
+
+            Weather weather2 = new Weather();
+            weather2.UpdateWeather();
+            weather2.DisplayWeather();
+
+            Weather weather3 = new Weather();
+            weather3.UpdateWeather();
+            weather3.DisplayWeather();
+
+            Weather weather4 = new Weather();
+            weather4.UpdateWeather();
+            weather4.DisplayWeather();
+
+            Weather weather5 = new Weather();
+            weather5.UpdateWeather();
+            weather5.DisplayWeather();
+
+            Weather weather6 = new Weather();
+            weather6.UpdateWeather();
+            weather6.DisplayWeather();
+
+            Weather weather7 = new Weather();
+            weather7.UpdateWeather();
+            weather7.DisplayWeather();
+
+            Weather weather8 = new Weather();
+            weather8.UpdateWeather();
+            weather8.DisplayWeather();
+
+            Weather weather9 = new Weather();
+            weather9.UpdateWeather();
+            weather9.DisplayWeather();
+
+            Weather weather10 = new Weather();
+            weather10.UpdateWeather();
+            weather10.DisplayWeather();
+
+            Weather weather11 = new Weather();
+            weather11.UpdateWeather();
+            weather11.DisplayWeather();
+
+            Weather weather12 = new Weather();
+            weather12.UpdateWeather();
+            weather12.DisplayWeather();
+
+            Weather weather13 = new Weather();
+            weather13.UpdateWeather();
+            weather13.DisplayWeather();
+
+            Weather weather14 = new Weather();
+            weather14.UpdateWeather();
+            weather14.DisplayWeather();
+
+            Weather weather15 = new Weather();
+            weather15.UpdateWeather();
+            weather15.DisplayWeather();
+
+            Weather weather16 = new Weather();
+            weather16.UpdateWeather();
+            weather16.DisplayWeather();
+
+            Weather weather17 = new Weather();
+            weather17.UpdateWeather();
+            weather17.DisplayWeather();
+
+            Weather weather18 = new Weather();
+            weather18.UpdateWeather();
+            weather18.DisplayWeather();
+
+            Weather weather19 = new Weather();
+            weather19.UpdateWeather();
+            weather19.DisplayWeather();
+
+            Weather weather20 = new Weather();
+            weather20.UpdateWeather();
+            weather20.DisplayWeather();
+
+        }
+
+
+    }
